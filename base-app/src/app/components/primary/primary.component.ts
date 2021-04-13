92b05465788d54a1316cf87ddcf976b6a69f1cb4
unknown <dmadrigal0696@gmail.com>
2021-04-12 20:45:49 -0600

    cambiar tamaño de arreglo

--- base-app/src/app/components/primary/primary.component.ts
+++ base-app/src/app/components/primary/primary.component.ts
@@ -9,7 +9,7 @@ import { DataService } from 'src/app/services/data/data.service';
 export class PrimaryComponent implements OnInit {
 
   maxLength: number = 15;
-  lengthSizes=[8,9,10,11,12,13,14,15,16,17,18];
+  lengthSizes=[2,3,4,5,6,7,8,9,10];
   namesList:{name:string}[]
   constructor(private dataService:DataService) {
     this.namesList = this.dataService.getNames();
--- base-app/src/app/pipes/small-names-pipe/small-names-pipe.pipe.ts
+++ base-app/src/app/pipes/small-names-pipe/small-names-pipe.pipe.ts
@@ -5,8 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SmallNamesPipe implements PipeTransform {
 
+
   transform(namesList:{name:string}[],maxLength:number): {name:string}[] {
-    return namesList.filter(value=> value.name && value.name.length<= maxLength);
+    let vowels = /[aeiou]/gi;
+    let newList:{name:string}[]=[];
+    namesList.forEach(element => {
+      var name = element.name;
+      var contador = 0;
+        for (let index = 0; index < name.length; index++) {
+          if(name[index]=='a' || name[index]=='e' || name[index]=='i' || name[index]=='o' || name[index]=='u'){
+            contador++;
+          }
+          const element = name[index];
+        }
+        
+        if(contador<maxLength){
+          newList.push(element);
+        }
+      });
+      return newList;
+  //  return namesList.filter( value=> value.name && value.name.length <= maxLenght)
   }
 
 }
